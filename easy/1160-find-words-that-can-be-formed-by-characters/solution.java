522ebd116cb2f11aff5a7cf7824c287cb3a7f2c9
Akhilbellana <akhilbellana22@gmail.com>
2026-08-06 23:47:58 +0530

    ✅ Find Words That Can Be Formed by Characters | Easy | Java | Runtime: 0 ms | Memory: 41.8 MB | 2026-08-06

--- easy/1160-find-words-that-can-be-formed-by-characters/solution.java
+++ easy/1160-find-words-that-can-be-formed-by-characters/solution.java
@@ -1,17 +1,23 @@
-        int[] freq=new int[26];
-        for(char ch:chars.toCharArray()){
-            freq[ch-'a']++;
-        }
-        int res=0;
-        for(String word : words){
-            int[] temp=freq.clone();
-            for(char ch:word.toCharArray()){
-                if(temp[ch-'a']==0){
-                    valid=false;
-                    break;
-                }
-            }
-            boolean valid=true;
-            if(valid){
-                res+=word.length();
-            }
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int[] freq=new int[26];
+        for(char ch:chars.toCharArray()){
+            freq[ch-'a']++;
+        }
+        int res=0;
+        for(String word : words){
+            boolean valid=true;
+            int[] temp=freq.clone();
+            for(char ch:word.toCharArray()){
+                if(temp[ch-'a']==0){
+                    valid=false;
+                    break;
+                }
+            }
+            if(valid){
+                res+=word.length();
+            }
+        }
+        return res;
+    }
+}
