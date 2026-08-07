1a7818a536187383841a4478fbc1245110a94133
Akhilbellana <akhilbellana22@gmail.com>
2026-08-07 14:26:14 +0530

    ✅ Word Pattern | Easy | Java | Runtime: 1 ms (83.66%) | Memory: 42.9 MB | 2026-08-07

--- easy/0290-word-pattern/solution.java
+++ easy/0290-word-pattern/solution.java
@@ -1,16 +1,29 @@
-        }
-            }
-              map1.put(words[i],pattern.charAt(i));  
-            }else{
-                }    
-                    return false;
-                if(!(map1.get(words[i])).equals(pattern.charAt(i))){
-            if(map1.containsKey(words[i])){
-        }
-        for(int i=0;i<words.length;i++){
-            return false;
-        if(words.length!=pattern.length()){
-        String[] words=s.split(" ");
-        Map<String,Character>map1=new HashMap<>();
-    public boolean wordPattern(String pattern, String s) {
-class Solution {
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<String,Character>map1=new HashMap<>();
+        String[] words=s.split(" ");
+        if(words.length!=pattern.length()){
+            return false;
+        }
+        for(int i=0;i<words.length;i++){
+            if(map1.containsKey(words[i])){
+                if(!(map1.get(words[i])).equals(pattern.charAt(i))){
+                    return false;
+                }    
+            }else{
+              map1.put(words[i],pattern.charAt(i));  
+            }
+        }
+        Map<Character,String>map2=new HashMap<>();
+        for(int i=0;i<words.length;i++){
+            if(map2.containsKey(pattern.charAt(i))){
+                if(!(map2.get(pattern.charAt(i)).equals(words[i]))){
+                    return false;
+                }
+            }else{
+                map2.put(pattern.charAt(i),words[i]);
+            }
+        }
+        return true;
+    }
+}
