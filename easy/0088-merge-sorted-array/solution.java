0c951a69d7f20875d499e41ed38e694bcad86c15
Akhilbellana <akhilbellana22@gmail.com>
2026-08-08 08:41:10 +0530

    ✅ Merge Sorted Array | Easy | Java | Runtime: 0 ms | Memory: 43.1 MB | 2026-08-08

--- easy/0088-merge-sorted-array/solution.java
+++ easy/0088-merge-sorted-array/solution.java
@@ -1,17 +1,15 @@
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
-        int j=n-1;
         int k=m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[j];
             }else{
                 nums1[i]=nums2[j];
-                k--;
-
             }
             i--;
             j--;
                 k--;
         }
 
+                nums1[k]=nums1[i];
+    }
+}
