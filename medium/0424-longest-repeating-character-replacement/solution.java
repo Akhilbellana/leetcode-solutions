df7f7e4270f49893bbf16d741a451241915b0187
Akhilbellana <akhilbellana22@gmail.com>
2026-08-10 23:55:20 +0530

    ✅ Longest Repeating Character Replacement | Medium | Java | Runtime: 27 ms (24.17%) | Memory: 47.3 MB | 2026-08-10

--- medium/0424-longest-repeating-character-replacement/solution.java
+++ medium/0424-longest-repeating-character-replacement/solution.java
@@ -1,18 +1,24 @@
-            int count=map.getOrDefault(s.charAt(j),0)+1;
-            map.put(s.charAt(j),count);
-            maxcount=Math.max(maxcount,count);
-            while((j-i+1)-maxcount>k){
-                map.put(s.charAt(i),map.get(s.charAt(i))-1);
-            }
-                if(map.get(s.charAt(i))==0){
-                    map.remove(s.charAt(i));
-                }
-                i++;
-        while(j<s.length()){
-        int maxlen=0;
-        int maxcount=0;
-        int j=0;
-        int i=0;
-        Map<Character, Integer>map=new HashMap<>();
-    public int characterReplacement(String s, int k) {
-            maxlen=Math.max(maxlen,j-i+1);
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer>map=new HashMap<>();
+        int i=0;
+        int j=0;
+        int maxcount=0;
+        int maxlen=0;
+        while(j<s.length()){
+            int count=map.getOrDefault(s.charAt(j),0)+1;
+            map.put(s.charAt(j),count);
+            maxcount=Math.max(maxcount,count);
+            while((j-i+1)-maxcount>k){
+                map.put(s.charAt(i),map.get(s.charAt(i))-1);
+                if(map.get(s.charAt(i))==0){
+                    map.remove(s.charAt(i));
+                }
+                i++;
+            }
+            maxlen=Math.max(maxlen,j-i+1);
+            j++;
+        }
+        return maxlen;
+    }
+}
