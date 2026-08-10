5b665403781559d50801ed61c8a75f3dc6e1a723
Akhilbellana <akhilbellana22@gmail.com>
2026-08-11 00:02:00 +0530

    ✅ Longest Repeating Character Replacement | Medium | Java | Runtime: 8 ms (66.46%) | Memory: 46.2 MB | 2026-08-10

--- medium/0424-longest-repeating-character-replacement/solution.java
+++ medium/0424-longest-repeating-character-replacement/solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        Map<Character, Integer>map=new HashMap<>();
         int i=0;
         int j=0;
-        int maxcount=0;
+        int maxfreq=0;
         int maxlen=0;
+        int[] freq=new int[26];
         while(j<s.length()){
-            int count=map.getOrDefault(s.charAt(j),0)+1;
-            map.put(s.charAt(j),count);
-            maxcount=Math.max(maxcount,count);
-            while((j-i+1)-maxcount>k){
-                map.put(s.charAt(i),map.get(s.charAt(i))-1);
-                if(map.get(s.charAt(i))==0){
-                    map.remove(s.charAt(i));
-                }
+           freq[s.charAt(j)-'A']++;
+           maxfreq=Math.max(maxfreq,freq[s.charAt(j)-'A']);
+            while((j-i+1)-maxfreq>k){
+                freq[s.charAt(i)-'A']--;
                 i++;
-            }
+                }  
             maxlen=Math.max(maxlen,j-i+1);
             j++;
         }
