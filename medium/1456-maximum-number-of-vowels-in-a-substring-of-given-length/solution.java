d1bfc493ccc017b1d3595ec541a18767b5040c65
Akhilbellana <akhilbellana22@gmail.com>
2026-08-10 16:14:44 +0530

    ✅ Maximum Number of Vowels in a Substring of Given Length | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-10

---
+++ medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/solution.java
@@ -0,0 +1,16 @@
+                if(b=='a'||b=='e'||b=='i'||b=='o'||b=='u'||b=='A'||b=='E'||b=='I'||b=='O'||b=='U'){
+                    v--;
+                max=Math.max(max,v);
+                char b=s.charAt(i);
+            if(j+i-1==k){
+            }
+                v++;
+            if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+            char a=s.charAt(j);
+        while(j<s.length()){
+        int max=0;
+        int v=0;
+        int j=0;
+        int i=0;
+    public int maxVowels(String s, int k) {
+class Solution {
