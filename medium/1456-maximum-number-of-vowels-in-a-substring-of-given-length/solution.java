d9041b271345d06918e696ac25bd8a0274f5a9aa
Akhilbellana <akhilbellana22@gmail.com>
2026-08-10 16:20:12 +0530

    ✅ Maximum Number of Vowels in a Substring of Given Length | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-10

--- medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/solution.java
+++ medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/solution.java
@@ -1,18 +1,25 @@
-                if(b=='a'||b=='e'||b=='i'||b=='o'||b=='u'||b=='A'||b=='E'||b=='I'||b=='O'||b=='U'){
-                    v--;
-                    i++;
-                }else{
-                    i++;
-                }
-            }
-            j++;
-            if(j-i+1==k){
-                char b=s.charAt(i);
-                max=Math.max(max,v);
-            }
-                v++;
-            if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
-            char a=s.charAt(j);
-        while(j<s.length()){
-        }
-        return max;
+class Solution {
+    public int maxVowels(String s, int k) {
+        int i=0;
+        int j=0;
+        int v=0;
+        int max=0;
+        while(j<s.length()){
+            char a=s.charAt(j);
+            if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+                v++;
+            }
+            if(j-i+1==k){
+                char b=s.charAt(i);
+                max=Math.max(max,v);
+                if(b=='a'||b=='e'||b=='i'||b=='o'||b=='u'||b=='A'||b=='E'||b=='I'||b=='O'||b=='U'){
+                    v--;
+                }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return max;
+    }
+}
