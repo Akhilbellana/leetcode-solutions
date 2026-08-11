6bcb826cd94a607be944617a3795a98f8374f9f7
Akhilbellana <akhilbellana22@gmail.com>
2026-08-11 18:22:13 +0530

    ✅ Next Greater Element I | Easy | Java | Runtime: 3 ms (92.67%) | Memory: 45.1 MB | 2026-08-11

--- easy/0496-next-greater-element-i/solution.java
+++ easy/0496-next-greater-element-i/solution.java
@@ -4,11 +4,18 @@ class Solution {
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
                 if(nums1[i]==nums2[j]){
-                    if(j+1<nums2.length&& nums2[j+1]>nums1[i] ){
-                        list.add(nums2[j+1]);
-                    }else{
+                    int k=j+1;
+                    while(k<nums2.length){
+                        if(nums2[k]>nums1[i]){
+                            list.add(nums2[k]);
+                            break;
+                        }
+                        k++;
+                    }
+                    if(k==nums2.length){
                         list.add(-1);
                     }
+                    break;
                 }
             }
         }
