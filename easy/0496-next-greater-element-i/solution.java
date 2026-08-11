43972ae5886f220b626640e1c2bcef60c61c2b51
Akhilbellana <akhilbellana22@gmail.com>
2026-08-11 18:09:52 +0530

    ✅ Next Greater Element I | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-11

---
+++ easy/0496-next-greater-element-i/solution.java
@@ -0,0 +1,18 @@
+                }
+            }
+        }
+                        list.add(nums2[j+1]);
+                    }else{
+                        list.add(-1);
+                    }
+        for(int i=0;i<list.size();i++){
+            a[i]=list.get(i);
+        }
+        int[] a=new int[list.size()];
+                    if(j+1<nums2.length&& nums2[j+1]>nums1[i] ){
+                if(nums1[i]==nums2[j]){
+        for(int i=0;i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+        List<Integer>list=new ArrayList<>();
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+class Solution {
