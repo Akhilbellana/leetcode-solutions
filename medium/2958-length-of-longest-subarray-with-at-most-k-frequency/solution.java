b6c684550024b305b25c972f28ac508b73a4b4a3
Akhilbellana <akhilbellana22@gmail.com>
2026-08-12 07:47:42 +0530

    ✅ Length of Longest Subarray With at Most K Frequency | Medium | Java | Runtime: 76 ms (18.74%) | Memory: 88.9 MB | 2026-08-12

--- medium/2958-length-of-longest-subarray-with-at-most-k-frequency/solution.java
+++ medium/2958-length-of-longest-subarray-with-at-most-k-frequency/solution.java
@@ -1,18 +1,21 @@
-        int j=0;
-        while(j<nums.length){
-           map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-        }
-           while(map.get(nums[j])>k){
-            map.put(nums[i],map.get(nums[i])-1);
-           }
-            if(map.get(nums[i])==0){
-                map.remove(nums[i]);
-            }
-            i++;
-           len=Math.max(len,j-i+1);
-        int len=0;
-           j++;
-        int i=0;
-        Map<Integer,Integer>map=new HashMap<>();
-    public int maxSubarrayLength(int[] nums, int k) {
-class Solution {
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        Map<Integer,Integer>map=new HashMap<>();
+        int i=0;
+        int j=0;
+        int len=0;
+        while(j<nums.length){
+           map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+           while(map.get(nums[j])>k){
+            map.put(nums[i],map.get(nums[i])-1);
+            if(map.get(nums[i])==0){
+                map.remove(nums[i]);
+            }
+            i++;
+           }
+           len=Math.max(len,j-i+1);
+           j++;
+        }
+        return len;
+    }
+}
