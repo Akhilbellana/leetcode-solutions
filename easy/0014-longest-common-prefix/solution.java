c20ce460ea243436f1878db79968c8932c22c487
Akhilbellana <akhilbellana22@gmail.com>
2026-08-12 15:40:00 +0530

    ✅ Longest Common Prefix | Easy | Java | Runtime: 2 ms | Memory: 42.8 MB | 2026-08-12

---
+++ easy/0014-longest-common-prefix/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        for(int i=0;i<strs.length;i++){
+            for(int j=0;j<strs.length;j++){
+                  if(strs[0].charAt(i)==strs[j].charAt(i)){
+            }
+            }
+        
+        String s="";
+                    s+=strs[0].charAt(i);
+                  }else{
+                    return "";
+                  }
+    }
+        return s;
+}
