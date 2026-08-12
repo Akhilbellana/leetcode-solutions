c383ca77819fdb506faa48e4141bd8c5245f48c6
Akhilbellana <akhilbellana22@gmail.com>
2026-08-12 23:49:43 +0530

    ✅ String Compression | Medium | Java | Runtime: 2 ms (27.52%) | Memory: 45.3 MB | 2026-08-12

--- medium/0443-string-compression/solution.java
+++ medium/0443-string-compression/solution.java
@@ -1,20 +1,25 @@
-                count++;
-            }else{
-                sb.append(chars[i]);
-                if(count>1){
-                   sb.append(count);
-                }
-            if(chars[i]==chars[i+1]){
-                   count=1;
-            }
-       } 
-       for(int i=0;i<chars.length-1;i++){
-       sb.append(chars[chars.length-1]);
-       if(count>1){
-        sb.append(count);
-       }
-       for(int i=0;i<sb.length();i++){
-        int count=1;
-        StringBuilder sb=new StringBuilder();
-    public int compress(char[] chars) {
-class Solution {
+class Solution {
+    public int compress(char[] chars) {
+        StringBuilder sb=new StringBuilder();
+        int count=1;
+       for(int i=0;i<chars.length-1;i++){
+            if(chars[i]==chars[i+1]){
+                count++;
+            }else{
+                sb.append(chars[i]);
+                if(count>1){
+                   sb.append(count);
+                   count=1;
+                }
+            }
+       } 
+       sb.append(chars[chars.length-1]);
+       if(count>1){
+        sb.append(count);
+       }
+       for(int i=0;i<sb.length();i++){
+        chars[i]=sb.charAt(i);
+       }
+       return sb.length();
+    }
+}
