dcb46a968222f2f0d50662c81e27ecb1920e7660
Akhilbellana <akhilbellana22@gmail.com>
2026-08-14 01:18:46 +0530

    ✅ Subarray Sum Equals K | Medium | Java | Runtime: 24 ms (76.15%) | Memory: 48.8 MB | 2026-08-13

--- medium/0560-subarray-sum-equals-k/solution.java
+++ medium/0560-subarray-sum-equals-k/solution.java
@@ -1,21 +1,18 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer,Integer>map=new HashMap<>();
-        map.put(0,-1);
+        map.put(0,1);
+        int count=0;
         int sum=0;
-        int len=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
             if(map.containsKey(sum-k)){
-                int prev=map.get(sum-k);
-                len=Math.max(len,i-prev);
+            count +=map.get(sum-k);
             }
-            if(!map.containsKey(sum)){
-                map.put(sum,i);
-            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         
  
         }
-        return len;
+        return count;
     }
 }
