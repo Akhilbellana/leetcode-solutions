c17405277267962c5a6d3b1b7bb2c26b2d2d5b61
Akhilbellana <akhilbellana22@gmail.com>
2026-08-13 14:19:03 +0530

    ✅ Best Time to Buy and Sell Stock | Easy | Java | Runtime: 2 ms (42.34%) | Memory: 94.1 MB | 2026-08-13

--- easy/0121-best-time-to-buy-and-sell-stock/solution.java
+++ easy/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int min=prices[0];
         int max=0;
-        for(int i=0;i<prices.length;i++){
-            for(int j=i+1;j<prices.length;j++){
-                if(prices[j]>prices[i]){
-                 max=Math.max(max,prices[j]-prices[i]);
-                }
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<min){
+                min=prices[i];
+            }else{
+                max=Math.max(max,prices[i]-min);
             }
-        }
+            }
         return max;
     }
 }
