d46b02806e4534a6c4558704274db61b3ab93cd5
Akhilbellana <akhilbellana22@gmail.com>
2026-08-14 13:54:17 +0530

    ✅ Find Pivot Index | Easy | Java | Runtime: 314 ms (7.68%) | Memory: 47.5 MB | 2026-08-14

--- easy/0724-find-pivot-index/solution.java
+++ easy/0724-find-pivot-index/solution.java
@@ -1,19 +1,19 @@
-class Solution {
-    public int pivotIndex(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-          if(lsum==rsum){
-        }
-        
-            int lsum=0;
-            for(int j=0;j<i;j++){
-                lsum+=nums[j];
-            }
-            int rsum=0;
-            for(int k=i+1;k<nums.length;k++){
-                rsum+=nums[k];
-            } 
-            return i;
-          }
-    }
-        return -1;
+class Solution {
+    public int pivotIndex(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int lsum=0;
+            for(int j=0;j<i;j++){
+                lsum+=nums[j];
+            }
+            int rsum=0;
+            for(int k=i+1;k<nums.length;k++){
+                rsum+=nums[k];
+            } 
+          if(lsum==rsum){
+            return i;
+          }
+        }
+        return -1;
+        
+    }
 }
