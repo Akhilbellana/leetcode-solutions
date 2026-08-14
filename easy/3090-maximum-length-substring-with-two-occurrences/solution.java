7dae815a202f7748663be6f7c99b1fa541125591
Akhilbellana <akhilbellana22@gmail.com>
2026-08-14 14:45:17 +0530

    ✅ Maximum Length Substring With Two Occurrences | Easy | Java | Runtime: 1 ms (100.00%) | Memory: 43.5 MB | 2026-08-14

--- easy/3090-maximum-length-substring-with-two-occurrences/solution.java
+++ easy/3090-maximum-length-substring-with-two-occurrences/solution.java
@@ -1,19 +1,19 @@
-class Solution {
-    public int maximumLengthSubstring(String s) {
-        int[] freq=new int[26];
-        int i=0;
-        int j=0;
-        int len=0;
-        while(j<s.length()){
-            freq[s.charAt(j)-'a']++;
-            while(freq[s.charAt(j)-'a']>2){
-               freq[s.charAt(i)-'a']--;
-                 i++;
-               
-            }
-            len=Math.max(len,j-i+1);
-            j++;
-        }
-        return len;
-    }
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int[] freq=new int[26];
+        int i=0;
+        int j=0;
+        int len=0;
+        while(j<s.length()){
+            freq[s.charAt(j)-'a']++;
+            while(freq[s.charAt(j)-'a']>2){
+               freq[s.charAt(i)-'a']--;
+                 i++;
+               
+            }
+            len=Math.max(len,j-i+1);
+            j++;
+        }
+        return len;
+    }
 }
