5a2292e3ea6eef198733df141694d1f94e08aabe
Akhilbellana <akhilbellana22@gmail.com>
2026-08-14 10:47:32 +0530

    ✅ Maximum Length Substring With Two Occurrences | Easy | Java | Runtime: 3 ms (43.02%) | Memory: 44.2 MB | 2026-08-14

--- easy/3090-maximum-length-substring-with-two-occurrences/solution.java
+++ easy/3090-maximum-length-substring-with-two-occurrences/solution.java
@@ -8,9 +8,9 @@ class Solution {
             map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
             while(map.get(s.charAt(j))>2){
                map.put(s.charAt(i),map.get(s.charAt(i))-1);
-               if(map.get(s.charAt(i))==0){
-                map.remove(s.charAt(i));
-               }
+            //    if(map.get(s.charAt(i))==0){
+            //     map.remove(s.charAt(i));
+            //    }
                  i++;
                
             }
