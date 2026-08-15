5314801b859e51522b82e103e46f33a738e93da7
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-15 18:38:44 +0530

    ✅ Power of Two | Easy | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-08-15

--- easy/0231-power-of-two/solution.java
+++ easy/0231-power-of-two/solution.java
@@ -1,8 +1,8 @@
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        if((n & (n-1))==0){
-            return true;
-        }
-    }
-            return false;
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if((n & (n-1))==0){
+            return true;
+        }
+            return false;
+    }
 }
