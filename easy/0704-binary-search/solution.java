f0b40f76a94522f186a5f247b41d72648dfab079
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-15 18:46:09 +0530

    ✅ Binary Search | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-15

---
+++ easy/0704-binary-search/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+        }
+            i=mid+1;
+           }else{
+               j=mid-1;
+           }
+            if(target==nums[mid]){
+                return mid;
+            }else if(target>nums[mid]){
+    }
+        return -1;
+            int mid=(i+j)/2;
+}
