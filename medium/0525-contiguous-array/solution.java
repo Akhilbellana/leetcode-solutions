88992e6ad13aadb36652f5b5593adf51f974f01a
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-16 18:42:17 +0530

    ✅ Contiguous Array | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-16

--- medium/0525-contiguous-array/solution.java
+++ medium/0525-contiguous-array/solution.java
@@ -1,19 +1,20 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-      Map<Integer,Integer>map=new HashMap<>();
-      int max=0;
-      int sum=0;
-      map.put(0,-1);
+        int max=0;
       for(int i=0;i<nums.length;i++){
-        sum+=nums[i];
-        if(map.containsKey(sum-i)){
-            max=Math.max(max,i-map.get(sum-i));
-        }
-        if(!map.containsKey(sum-i)){
-            map.put(sum,i);
+        int z=0;
+        int o=0;
+        for(int j=i;j<nums.length;j++){
+            if(nums[j]==0){
+                z++;
+            }else{
+                o++;
+            }
+            if(z==o){
+                max=Math.max(max,j-i+1);
+            }
         }
       }
-      return max;
-      
+      return max;  
     }
 }
