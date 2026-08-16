d636f6bd096d055ad5eec81dbcb981a295cf5dbe
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-16 18:56:59 +0530

    ✅ Contiguous Array | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-16

--- medium/0525-contiguous-array/solution.java
+++ medium/0525-contiguous-array/solution.java
@@ -6,10 +6,10 @@ class Solution {
       map.put(0,-1);
       for(int i=0;i<nums.length;i++){
         sum+=nums[i];
-        if(map.containsKey(sum-i)){
-            max=Math.max(max,i-map.get(sum-i));
+        if(map.containsKey(i-sum)){
+            max=Math.max(max,i-map.get(i-sum));
         }
-        if(!map.containsKey(sum-i)){
+        if(!map.containsKey(i-sum)){
             map.put(sum,i);
         }
       }
