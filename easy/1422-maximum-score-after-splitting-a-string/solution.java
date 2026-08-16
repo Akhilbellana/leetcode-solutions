339a51a045bfabacb586db863615c39e7def201c
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-16 11:28:44 +0530

    ✅ Maximum Score After Splitting a String | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-16

--- easy/1422-maximum-score-after-splitting-a-string/solution.java
+++ easy/1422-maximum-score-after-splitting-a-string/solution.java
@@ -1,20 +1,21 @@
-        if(s.charAt(i)=='1'){
-            ones++;
-        }
-       }
-       for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='0'){
-       }
-            zeros++;
-        }else{
-            ones--;
-        }
-    }
-        max=Math.max(max,zeros+ones);
-       int max=0;
-       return max;
-       for(int i=0;i<s.length();i++){
-        int ones=0;
-        int zeros=0;
-class Solution {
-    public int maxScore(String s) {
+class Solution {
+    public int maxScore(String s) {
+        int ones=0;
+        int zeros=0;
+       for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='1'){
+            ones++;
+        }
+       }
+       int max=0;
+       for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='0'){
+            zeros++;
+        }else{
+            ones--;
+        }
+        max=Math.max(max,zeros+ones);
+       }
+       return max;
+    }
+}
