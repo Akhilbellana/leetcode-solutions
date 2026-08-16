9df24beb1ac53d04a191f54f5de37b443e50b6a4
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-16 19:25:22 +0530

    ✅ Binary Subarrays With Sum | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-16

---
+++ medium/0930-binary-subarrays-with-sum/solution.java
@@ -0,0 +1,20 @@
+        int sum=0;
+        while(j<nums.length){
+            sum+=nums[j];
+        }
+            while(sum>goal){
+                if(nums[i]==1){
+            }
+                    sum--;
+                }
+                i++;
+            j++;
+            if(sum==goal){
+                count++;
+            }
+        return count;
+        int j=0;
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int i=0;
+class Solution {
+        int count=0;
