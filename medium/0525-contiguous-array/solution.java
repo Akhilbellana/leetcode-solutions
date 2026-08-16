6e34950eb70e51f5bb5077ce71466ebb841d14f3
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-16 18:34:01 +0530

    ✅ Contiguous Array | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-16

--- medium/0525-contiguous-array/solution.java
+++ medium/0525-contiguous-array/solution.java
@@ -1,20 +1,20 @@
-      for(int i=0;i<nums.length;i++){
-        for(int j=i;j<nums.length;j++){
-            if(nums[j]==0){
-                z++;
-            }else{
-        }
-      }
-                o++;
-            }
-            if(z==o){
-                max=Math.max(max,j-i+1);
-            }
-      return max;  
-        int max=0;
-        int z=0;
-        int o=0;
-    public int findMaxLength(int[] nums) {
-class Solution {
-    }
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int max=0;
+      for(int i=0;i<nums.length;i++){
+        int z=0;
+        int o=0;
+        for(int j=i;j<nums.length;j++){
+            if(nums[j]==0){
+                z++;
+            }else{
+                o++;
+            }
+            if(z==o){
+                max=Math.max(max,j-i+1);
+            }
+        }
+      }
+      return max;  
+    }
 }
