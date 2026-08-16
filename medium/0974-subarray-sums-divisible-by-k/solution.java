38f62c3cd54ddf788a60fc39256e9d18651c8dde
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-16 16:44:54 +0530

    ✅ Subarray Sums Divisible by K | Medium | Java | Runtime: 0 ms | Memory: 42.3 MB | 2026-08-16

--- medium/0974-subarray-sums-divisible-by-k/solution.java
+++ medium/0974-subarray-sums-divisible-by-k/solution.java
@@ -6,12 +6,12 @@ class Solution {
      int count=0;
      for(int i=0;i<nums.length;i++){
         sum+=nums[i];
-        int valid=sum%k;
+        int valid=((sum%k)+k)%k;
         if(map.containsKey(valid)){
             count+=map.get(valid);
         }
         map.put(sum%k,map.getOrDefault(sum%k,0)+1);
      }
-     return count++;
+     return count;
     }
 }
