318134db9969a5024593397b51454beb1027839b
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-17 14:39:54 +0530

    ✅ Majority Element II | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-17

--- medium/0229-majority-element-ii/solution.java
+++ medium/0229-majority-element-ii/solution.java
@@ -1,15 +1,16 @@
-    public List<Integer> majorityElement(int[] nums) {
-        List<Integer>list=new ArrayList<>();
-        Map<Integer,Integer>map=new HashMap<>();
         int n=nums.length;
         for(int i=0;i<n;i++){
             if(map.containsKey(nums[i])){
-        }
                 if(map.get(nums[i])>n/3){
             }
                     if(!list.contains(nums[i])){
                 }
                         list.add(nums[i]);
                     }
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        Map<Integer,Integer>map=new HashMap<>();
+        List<Integer>list=new ArrayList<>();
+    public List<Integer> majorityElement(int[] nums) {
 class Solution {
