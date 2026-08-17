27a6dcee3b862edf5e163562022b970855bf7230
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-17 17:17:29 +0530

    ✅ Minimum Positive Sum Subarray | Easy | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-08-17

---
+++ easy/3364-minimum-positive-sum-subarray/solution.java
@@ -0,0 +1,27 @@
+            if (j - i + 1 == l) {
+                if (sum > 0) {
+                    min = Math.min(min, sum);
+                }
+                i++;
+                sum -= nums.get(i);
+                sums = sum;
+                    sums += nums.get(x);
+                int x = j+1;
+
+                }
+                    if (sums > 0) {
+                        min = Math.min(min, sums);
+                    }
+                    x++;
+            sum += nums.get(j);
+        while(j < nums.size()) {
+        int min=Integer.MAX_VALUE;
+        int sums = 0;
+        int j = 0;
+                int y=0;
+                while(y<r-l && x<nums.size()){
+            }
+            j++;
+        }
+        return min;
+                    y++;
