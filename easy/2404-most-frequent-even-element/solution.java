a1094f82a6782a18d38c0e147b6af0f439a11521
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-17 15:13:31 +0530

    ✅ Most Frequent Even Element | Easy | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-08-17

--- easy/2404-most-frequent-even-element/solution.java
+++ easy/2404-most-frequent-even-element/solution.java
@@ -1,3 +1,5 @@
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
                 map.put(nums[i],map.getOrDefault(nums[i],0)+1);
             }
         }
@@ -5,18 +7,17 @@
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
                int num = entry.getKey();
             int freq = entry.getValue();
-        int val=-1;
+        int val=0;
                 if(freq>max){
                   val=num;
                 }else if(freq==max){
     
 }
         return val;
+        Map<Integer,Integer>map=new HashMap<>();
+    public int mostFrequentEven(int[] nums) {
                     val=Math.min(val,num);
                 }
     }
                     max=freq;
 }
-            if(nums[i]%2==0){
-        for(int i=0;i<nums.length;i++){
-        Map<Integer,Integer>map=new HashMap<>();
