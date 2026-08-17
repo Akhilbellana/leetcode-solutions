81c2f1b7a18013a69820ae1e04403a1c5daf3ddd
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-17 15:20:31 +0530

    ✅ Most Frequent Even Element | Easy | Java | Runtime: 17 ms (79.05%) | Memory: 47.6 MB | 2026-08-17

--- easy/2404-most-frequent-even-element/solution.java
+++ easy/2404-most-frequent-even-element/solution.java
@@ -1,23 +1,24 @@
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            }
-        }
-        int max=0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-               int num = entry.getKey();
-            int freq = entry.getValue();
-        int val=0;
-                if(freq>max){
-                  val=num;
-                }else if(freq==max){
-    
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            }
+        }
+        int max=0;
+        int val=-1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+               int num = entry.getKey();
+            int freq = entry.getValue();
+                if(freq>max){
+                    max=freq;
+                  val=num;
+                }else if(freq==max){
+                    val=Math.min(val,num);
+                }
+    
 }
-        return val;
-        Map<Integer,Integer>map=new HashMap<>();
-    public int mostFrequentEven(int[] nums) {
-                    val=Math.min(val,num);
-                }
-    }
-                    max=freq;
+        return val;
+    }
 }
