b4dac256b44adc89c5028e395728badfc8f45b83
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-18 14:26:56 +0530

    ✅ Find the Largest Almost Missing Integer | Easy | Java | Runtime: 5 ms (43.16%) | Memory: 46.1 MB | 2026-08-18

--- easy/3471-find-the-largest-almost-missing-integer/solution.java
+++ easy/3471-find-the-largest-almost-missing-integer/solution.java
@@ -1,23 +1,26 @@
-            for(int j=i;j<i+k;j++){
-               if(!set.contains(nums[j])){
-        }
-                set.add(nums[j]);
-               }
-                map.put(nums[j],map.getOrDefault(nums[j],0)+1);
-        }
-        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-            if(entry.getValue()==1){
-        }
-                max=Math.max(max,entry.getKey());
-            }
-                valid=true;
-        if(valid){
-        return max;
-        }
-        return -1;
-    }
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+    int max=0;
+    boolean valid=false;
+    Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<=nums.length-k;i++){
+            Set<Integer>set=new HashSet<>();
+            for(int j=i;j<i+k;j++){
+               if(!set.contains(nums[j])){
+                set.add(nums[j]);
+                map.put(nums[j],map.getOrDefault(nums[j],0)+1);
+               }
+        }
+        }
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+            if(entry.getValue()==1){
+                max=Math.max(max,entry.getKey());
+                valid=true;
+            }
+        }
+        if(valid){
+        return max;
+        }
+        return -1;
+    }
 }
-            Set<Integer>set=new HashSet<>();
-        for(int i=0;i<=nums.length-k;i++){
-    Map<Integer,Integer>map=new HashMap<>();
-    boolean valid=false;
