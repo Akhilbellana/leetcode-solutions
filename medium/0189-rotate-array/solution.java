5f5a38ffea875c3ced8c1a1965c77d7296a2b3eb
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-18 14:53:57 +0530

    ✅ Rotate Array | Medium | Java | Runtime: 7 ms (3.38%) | Memory: 273.2 MB | 2026-08-18

--- medium/0189-rotate-array/solution.java
+++ medium/0189-rotate-array/solution.java
@@ -1,13 +1,13 @@
-class Solution {
-    public void rotate(int[] nums, int k) {
-       int[]a=new int[nums.length];
-       for(int j=0;j<nums.length;j++){
-        int i=(j+k)%nums.length;
-        a[i]=nums[j];
-       }
-       for(int i=0;i<nums.length;i++){
-        nums[i]=a[i];
-       } 
-    }
-       k=k%nums.length;
+class Solution {
+    public void rotate(int[] nums, int k) {
+       int[]a=new int[nums.length];
+       k=k%nums.length;
+       for(int j=0;j<nums.length;j++){
+        int i=(j+k)%nums.length;
+        a[i]=nums[j];
+       }
+       for(int i=0;i<nums.length;i++){
+        nums[i]=a[i];
+       } 
+    }
 }
