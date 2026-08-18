f386c124a95fb7f100b30ca3cc369046c8400ce8
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-18 11:34:35 +0530

    ✅ Find the Largest Almost Missing Integer | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-08-18

---
+++ easy/3471-find-the-largest-almost-missing-integer/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int largestInteger(int[] nums, int k) {
+    int max=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i;j<nums.length;j++){
+                if(set.contains(nums[j])){
+            }
+        }
+                    max=Math.max(max,j-i+1);
+                }
+    }
+            Set<Integer>set=new HashSet<>();
+        return max;
+}
