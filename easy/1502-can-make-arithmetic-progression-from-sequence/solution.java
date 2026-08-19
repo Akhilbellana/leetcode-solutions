0437dc96ae3cb1c7b4f05c14a473aa9d969b5beb
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-20 00:58:27 +0530

    ✅ Can Make Arithmetic Progression From Sequence | Easy | Java | Runtime: 1 ms | Memory: 42.9 MB | 2026-08-19

---
+++ easy/1502-can-make-arithmetic-progression-from-sequence/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length-1;i++){
+            int j=i+1;
+            if(arr[j]-arr[i]!=d){
+                 return false;
+            }
+        }
+    }
+        int d=arr[1]-arr[0];
+        return true;
+}
