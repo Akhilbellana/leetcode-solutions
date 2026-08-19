1a8377d1430af124a72c519d0511d8bdbdba3a44
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-20 00:18:37 +0530

    ✅ Find the Pivot Integer | Easy | Java | Runtime: 0 ms | Memory: 41.9 MB | 2026-08-19

--- easy/2485-find-the-pivot-integer/solution.java
+++ easy/2485-find-the-pivot-integer/solution.java
@@ -1,13 +1,12 @@
 class Solution {
     public int pivotInteger(int n) {
-        int sum=0;
         int[] prefix=new int[n+1];
         for(int i=1;i<n;i++){
             prefix[i+1]=prefix[i]+i;
         }
         prefix[0]=0;
-        for(int i=0;i<n;i++){
-            if(prefix[i]==prefix[n]-prefix[i]){
+        for(int i=1;i<n+1;i++){
+            if(prefix[i]==prefix[n]-prefix[i-1]){
         }
                 return i;
             }
