75480174d2dc167b3c98bc86a6c9b4921a396ec1
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-20 00:10:22 +0530

    ✅ Find Pivot Index | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-08-19

--- easy/0724-find-pivot-index/solution.java
+++ easy/0724-find-pivot-index/solution.java
@@ -1,18 +1,18 @@
-class Solution {
-    public int pivotIndex(int[] nums) {
-        int[] prefix=new int[nums.length+1];
-        prefix[0]=0;
-        for(int i=0;i<nums.length;i++){
-            prefix[i+1]=prefix[i]+nums[i];
-        }
-            for(int i=0;i<nums.length;i++){
-            int left=prefix[i]-prefix[0];
-            int right=prefix[nums.length]-prefix[i+1];
-          if(left==right){
-            return i;
-          }
-        }
-        return -1;
-        
-    }
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] prefix=new int[nums.length+1];
+        prefix[0]=0;
+        for(int i=0;i<nums.length;i++){
+            prefix[i+1]=prefix[i]+nums[i];
+        }
+            for(int i=0;i<nums.length;i++){
+            int left=prefix[i];
+            int right=prefix[nums.length]-prefix[i+1];
+          if(left==right){
+            return i;
+          }
+        }
+        return -1;
+        
+    }
 }
