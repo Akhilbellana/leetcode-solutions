b5ac623ec2ba9831f3e8404c71c6467f84afc7ed
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-20 23:00:54 +0530

    ✅ Distribute Elements Into Two Arrays I | Easy | Java | Runtime: 0 ms | Memory: 43 MB | 2026-08-20

---
+++ easy/3069-distribute-elements-into-two-arrays-i/solution.java
@@ -0,0 +1,12 @@
+        for(int i=0;i<list1.size();i++){
+            a[i]=list1.get(i);
+        }
+       
+        for(int i=list1.size();i<nums.length;i++){
+             a[i]=list2.get(j);
+        }
+        int j=0;
+             j++;
+        return a;
+    }
+}
