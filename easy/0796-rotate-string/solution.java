2d6f44f4c2052f02362f12ed90443f55e78d4c49
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-20 17:18:26 +0530

    ✅ Rotate String | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-20

---
+++ easy/0796-rotate-string/solution.java
@@ -0,0 +1,18 @@
+            }
+        }
+         if(index!=-1){
+            for(int j=0;j<s.length();j++){
+                if(s.charAt((index+j)%s.length())!=goal.charAt(j)){       
+                    return false;
+                }
+            }
+            return true;
+                index=i;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==goal.charAt(0)){
+         int index=-1;
+        if(s.length()!=goal.length()){
+        return false;
+        }
+    }else{
+        return false;
