5a6a8c019ac7c47498f916135d6e9ad7ce63717a
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-20 22:39:06 +0530

    ✅ Sum of Variable Length Subarrays | Easy | Java | Runtime: 1 ms (99.89%) | Memory: 45.3 MB | 2026-08-19

--- easy/3427-sum-of-variable-length-subarrays/solution.java
+++ easy/3427-sum-of-variable-length-subarrays/solution.java
@@ -1,15 +1,15 @@
-class Solution {
-    public int subarraySum(int[] nums) {
-        int sum=0;
-        int[] prefix=new int[nums.length+1];
-        for(int i=0;i<nums.length;i++){
-            int start=Math.max(0,i-nums[i]);
-        }
-        return sum;
-        prefix[0]=0;
-            sum+=prefix[i+1]-prefix[start+1];
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-        }
-    }
+class Solution {
+    public int subarraySum(int[] nums) {
+        int sum=0;
+        int[] prefix=new int[nums.length+1];
+        prefix[0]=0;
+        for(int i=0;i<nums.length;i++){
+            prefix[i+1]=prefix[i]+nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            int start=Math.max(0,i-nums[i]);
+            sum+=prefix[i+1]-prefix[start];
+        }
+        return sum;
+    }
 }
