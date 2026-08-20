7fb282bc14d1530840b796bc295aee1b9803e078
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-20 23:51:19 +0530

    ✅ Three Consecutive Odds | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-20

---
+++ easy/1550-three-consecutive-odds/solution.java
@@ -0,0 +1,17 @@
+        while(arr.length>=3 && i<arr.length){
+            if(arr[i]%2!=0){
+
+        }
+                count++;
+            }else{
+                count=0;
+            }
+                if(count==3){
+                    return true;
+                }
+        int count=0;
+        int i=0;
+        return false;
+    public boolean threeConsecutiveOdds(int[] arr) {
+class Solution {
+            i++;
