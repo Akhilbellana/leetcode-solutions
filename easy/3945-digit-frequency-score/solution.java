7362f097745c7844302988a600c1f6109a85ca5c
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-21 01:38:03 +0530

    ✅ Digit Frequency Score | Easy | Java | Runtime: 3 ms (20.05%) | Memory: 43 MB | 2026-08-20

--- easy/3945-digit-frequency-score/solution.java
+++ easy/3945-digit-frequency-score/solution.java
@@ -1,17 +1,17 @@
-        Map<Integer, Integer> map = new HashMap<>();
-        while (n > 0) {
-            int digit = n % 10;
-            map.put(digit, map.getOrDefault(digit, 0) + 1);
-            n = n / 10;
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            sum+=value*key;   
-        }
-    }
-        return sum;
-}        
-        int sum = 0;
-    public int digitFrequencyScore(int n) {
-class Solution {
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        while (n > 0) {
+            int digit = n % 10;
+            map.put(digit, map.getOrDefault(digit, 0) + 1);
+            n = n / 10;
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            sum+=value*key;   
+        }
+        return sum;
+    }
+}        
