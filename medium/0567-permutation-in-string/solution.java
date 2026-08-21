88a448c8562fc4a69d2694e5f515fdbef1d418ad
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-21 21:27:06 +0530

    ✅ Permutation in String | Medium | Java | Runtime: 28 ms (23.18%) | Memory: 47 MB | 2026-08-21

--- medium/0567-permutation-in-string/solution.java
+++ medium/0567-permutation-in-string/solution.java
@@ -1,25 +1,35 @@
-                    if(values!=0){
-                }
-                         valid=false;
-                    }
-                         break;
-                if(valid){
-                    return true;
-                }else{
-                    if(map.containsKey(s2.charAt(i))){
-                for(int values:map.values()){
-            if(j-i+1==s1.length()){
-            }
-                map.put(s2.charAt(j),map.get(s2.charAt(j))-1);
-                boolean valid=true;
-            if(map.containsKey(s2.charAt(j))){
-        while(j<s2.length()){
-        int j=0;
-        int i=0;
-                        map.put(s2.charAt(i),map.get(s2.charAt(i))+1);
-                    }
-                    i++;
-                }
-            }
-            j++;
-        }
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        Map<Character,Integer>map=new HashMap<>();
+        for(char ch:s1.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int i=0;
+        int j=0;
+        while(j<s2.length()){
+            if(map.containsKey(s2.charAt(j))){
+                map.put(s2.charAt(j),map.get(s2.charAt(j))-1);
+            }
+            if(j-i+1==s1.length()){
+                boolean valid=true;
+                for(int values:map.values()){
+                    if(values!=0){
+                         valid=false;
+                         break;
+                    }
+                }
+                if(valid){
+                    return true;
+                }else{
+                    if(map.containsKey(s2.charAt(i))){
+                        map.put(s2.charAt(i),map.get(s2.charAt(i))+1);
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        return false;
+
+    }
+}
