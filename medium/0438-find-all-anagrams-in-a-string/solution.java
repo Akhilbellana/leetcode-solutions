352a98b77ef0809af6b93b5ae834e3f5c3a4cba5
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-21 22:46:33 +0530

    ✅ Find All Anagrams in a String | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-21

---
+++ medium/0438-find-all-anagrams-in-a-string/solution.java
@@ -0,0 +1,25 @@
+        for(int i=0;i<p.length();i++){
+            if(map.containsKey(s.charAt(i))&& valid){
+        }
+                temp=i;
+            }else if(map.containsKey(s.charAt(i))){
+        boolean valid=true;
+                valid=false;
+                 map.put(s.charAt(i),map.get(s.charAt(i))-1);
+            }
+                map.put(s.charAt(i),map.get(s.charAt(i))-1);
+            for(int values : map.values()){
+                if(values!=0){
+            }
+                    break;
+                }
+            valid=true;
+            list.add(temp);
+        }
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        for(char ch:s.toCharArray()){
+        List<Integer>list=new ArrayList<>();
+        return list;
+        
+        int temp=-1;
+    }
