2a64bb4d3cf0125365f23ee1fada6b7ee09f6c26
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-21 12:24:59 +0530

    ✅ Permutation in String | Medium | Java | Runtime: 6 ms (87.75%) | Memory: 44 MB | 2026-08-21

--- medium/0567-permutation-in-string/solution.java
+++ medium/0567-permutation-in-string/solution.java
@@ -1,32 +1,32 @@
-                    if (freq[k] != 0) {
-                        valid = false;
-                for (int k = 0; k < freq.length; k++) {
-                        break;
-                    }
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            freq[s1.charAt(i) - 'a']++;
+        }
+        int i = 0;
+        int j = 0;
+        while (j < s2.length()) {
+            freq[s2.charAt(j) - 'a']--;
+            if (j - i + 1 == s1.length()) {
+                boolean valid = true;
+                for (int k = 0; k < freq.length; k++) {
+                    if (freq[k] != 0) {
+                        valid = false;
+                        break;
+                    }
 
-                boolean valid = true;
-                }
-                if (valid) {
-                    return true;
-                } else {
-                    freq[s2.charAt(i) - 'a']++;
-                    i++;
-                }
-            j++;
-        }
-        return false;
-    }
-            if (j - i + 1 == s1.length()) {
-            freq[s2.charAt(j) - 'a']--;
-        while (j < s2.length()) {
-        int i = 0;
-        int j = 0;
-            freq[s1.charAt(i) - 'a']++;
-        }
-        for (int i = 0; i < s1.length(); i++) {
-        int[] freq = new int[26];
-    public boolean checkInclusion(String s1, String s2) {
-class Solution {
+                }
+                if (valid) {
+                    return true;
+                } else {
+                    freq[s2.charAt(i) - 'a']++;
+                    i++;
+                }
+            }
+            j++;
+        }
 
-            }
+        return false;
+    }
 }
