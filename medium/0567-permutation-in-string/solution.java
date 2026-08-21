8e6d0ca13bae41ef22ab0987f1c5b8a2ace678d4
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-21 11:51:53 +0530

    ✅ Permutation in String | Medium | Java | Runtime: 0 ms | Memory: 41.7 MB | 2026-08-21

---
+++ medium/0567-permutation-in-string/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] freq=new int[26];
+        for(int i=0;i<s1.length();i++){
+            freq[s1.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s2.length();i++){
+            freq[s2.charAt(i)-'a']--;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]>1){
+        }
+                return false;
+            }
+    }
+        return false;
