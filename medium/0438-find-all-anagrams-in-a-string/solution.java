b6beda5425855c04164cd7ee2c3ec5b2d4d66f71
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-21 23:22:48 +0530

    ✅ Find All Anagrams in a String | Medium | Java | Runtime: 5 ms (98.41%) | Memory: 46.9 MB | 2026-08-21

--- medium/0438-find-all-anagrams-in-a-string/solution.java
+++ medium/0438-find-all-anagrams-in-a-string/solution.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        Map<Character,Integer>map=new HashMap<>();
-        List<Integer>list=new ArrayList<>();
-        for(char ch:p.toCharArray()){
-            map.put(ch,map.getOrDefault(ch,0)+1);
+        int[] freq = new int[26];
+        List<Integer> list = new ArrayList<>();
+        for (char ch : p.toCharArray()) {
+            freq[ch - 'a']++;
         }
-        int i=0;
-        int j=0;
-        while(j<s.length()){
-            if(map.containsKey(s.charAt(j))){
-                map.put(s.charAt(j),map.get(s.charAt(j))-1);
-            }
-            if(j-i+1==p.length()){
-                boolean valid=true;
-                for(int values:map.values()){
-                    if(values!=0){
-                        valid=false;
+        int i = 0;
+        int j = 0;
+        while (j < s.length()) {
+            freq[s.charAt(j) - 'a']--;
+            if (j - i + 1 == p.length()) {
+                boolean valid = true;
+                for (int n : freq) {
+                    if (n != 0) {
+                        valid = false;
+                        break;
                     }
                 }
-                if(valid){
+                if (valid) {
                     list.add(i);
                 }
-                if(map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);
-                }
+                freq[s.charAt(i) - 'a']++;
                 i++;
             }
             j++;
         }
         return list;
-        
+
     }
 }
