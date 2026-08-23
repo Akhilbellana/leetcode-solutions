480ad7dbdb6c151bcbce321473ddcfcb017a5c9f
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-23 15:26:10 +0530

    ✅ Make Sum Divisible by P | Medium | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-08-23

--- medium/1590-make-sum-divisible-by-p/solution.java
+++ medium/1590-make-sum-divisible-by-p/solution.java
@@ -1,25 +1,23 @@
-        tot+=n;
-       }
-       int target=tot%p;
-       if(target==0){
         return 0;
-       }
-       if(tot<p){
-        return -1;
        }
        int sum=0;
        boolean valid=false;
        int min=Integer.MAX_VALUE;
        for(int i=0;i<nums.length;i++){
            sum+=nums[i];
-           if(map.containsKey(sum-target) && map.get(sum-target)%p==0){
+           if(map.containsKey(sum-target)){
             min=Math.min(min,i-map.get(sum-target));
             valid=true;
            }
-           map.put(sum,i);
+           map.put(sum%p,i);
        }
        if(valid){
         return min;
        }
        return -1;
 
+    }
+}
+       if(target==0){
+       int target=tot%p;
+       }
