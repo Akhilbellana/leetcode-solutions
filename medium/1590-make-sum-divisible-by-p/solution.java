d90e080c5cb6c3fd5e296d0998ece4eb713581c0
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-23 14:53:02 +0530

    ✅ Make Sum Divisible by P | Medium | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-23

--- medium/1590-make-sum-divisible-by-p/solution.java
+++ medium/1590-make-sum-divisible-by-p/solution.java
@@ -1,22 +1,24 @@
-class Solution {
-    public int minSubarray(int[] nums, int p) {
-       Map<Integer,Integer>map=new HashMap<>();
-       map.put(0,1);
-       int sum=0;
+       for(int n:nums){
+        tot+=n;
+       }
        for(int i=0;i<nums.length;i++){
-        sum+=nums[i];
-       } 
-        if(map.containsKey(sum%p)){
-            min=Math.min(min,i-map.get(sum%p));
-        }
-       int min=Integer.MAX_VALUE;
-        map.put(sum%p,i);
-        
-            valid=true;
-       boolean valid=false;
+           sum+=nums[i];
+       }
+       int target=tot%p;
+       int sum=0;
+           if(map.containsKey(sum-target)){
+            min=Math.min(min,i-map.get(sum-target));
+           }
+           map.put(sum,i);
        if(valid){
         return min;
-    }
        }
        return -1;
-}
+            valid=true;
+       boolean valid=false;
+       int tot=0;
+       map.put(0,-1);
+       Map<Integer,Integer>map=new HashMap<>();
+    public int minSubarray(int[] nums, int p) {
+class Solution {
+       int min=Integer.MAX_VALUE;
