deba0066c64f4190afb06638ff97aaf1cc7d6d86
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-24 12:20:17 +0530

    ✅ Length of Last Word | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-08-24

--- easy/0058-length-of-last-word/solution.java
+++ easy/0058-length-of-last-word/solution.java
@@ -1,7 +1,14 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words=s.split(" ");
-        int len=words[words.length-1].length();
-        return len;
+        s=s.trim();
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                len++;
+            }else{
+                return len;
+            }
+        }
+        return -1;
     }
 }
