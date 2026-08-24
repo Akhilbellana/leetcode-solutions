e102990586c898e339b9046fca5bd081d303809f
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-24 09:45:19 +0530

    ✅ Kth Missing Positive Number | Easy | Java | Runtime: 0 ms | Memory: 42.5 MB | 2026-08-24

---
+++ easy/1539-kth-missing-positive-number/solution.java
@@ -0,0 +1,21 @@
+        for(int i=0;i<arr.length;i++){
+            min=Math.min(min,arr[i]);
+        }
+        
+            max=Math.max(max,arr[i]);
+        for(int i= min;i<=max;i++){
+            if(!list.contains(i)){
+        }
+        int count=0;
+                count++;
+            }
+            if(count==k){
+                return i;
+            }
+        return -1;
+        List<Integer>list=new ArrayList<>();
+        for(int n:arr){
+            list.add(n);
+        }
+    }
+}
