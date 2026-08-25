4e0a8f7cef2a063158ba420e2c07a250986964ff
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-25 16:04:19 +0530

    ✅ Sort Colors | Medium | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-08-25

--- medium/0075-sort-colors/solution.java
+++ medium/0075-sort-colors/solution.java
@@ -1,16 +1,15 @@
-                    nums[curr]=nums[i];
-                    int temp2=nums[curr];
-                if(nums[curr]==0){
-                j--;
-                nums[j]=temp1;
-                nums[curr]=nums[j];
-                int temp1=nums[curr];
-            if(nums[curr]==2){
-                    nums[i]=temp2;
-                    i++;
+                    curr++;
                 }
             }else if(nums[curr]==0){
                 int temp3=nums[curr];
                 nums[curr]=nums[i];
                 nums[i]=temp3;
-                   curr++;
+                i++;
+            }else{
+                curr++;
+            }
+        }
+        
+    }
+                curr++;
+}
