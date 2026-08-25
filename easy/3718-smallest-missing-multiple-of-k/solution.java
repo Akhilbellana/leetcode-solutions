d2648ece830b09e39247b11c6762ce571fc73b9d
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-25 11:35:03 +0530

    ✅ Smallest Missing Multiple of K | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-25

--- easy/3718-smallest-missing-multiple-of-k/solution.java
+++ easy/3718-smallest-missing-multiple-of-k/solution.java
@@ -1,14 +1,9 @@
-class Solution {
-    public int missingMultiple(int[] nums, int k) {
-        Set<Integer>set=new HashSet<>();
         int i=k;
-        while(count>0){
+        while(count==0){
             if(i%k==0 && !set.contains(i)){
         }
         
         int count=0;
-        for(int n:nums){
-            set.add(n);
         }
                 count++;
             }
