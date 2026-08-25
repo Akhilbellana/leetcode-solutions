6ddd6a5936d28265f378d71878b15e461a9b0cad
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-25 16:01:07 +0530

    ✅ Sort Colors | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-25

--- medium/0075-sort-colors/solution.java
+++ medium/0075-sort-colors/solution.java
@@ -2,29 +2,13 @@
                 int temp1=nums[curr];
             if(nums[curr]==2){
         while(curr<j){
-        int j=nums.length-1;
-        int curr=0;
                 nums[j]=temp1;
                 j--;
                 if(nums[curr]==0){
                     int temp2=nums[curr];
                     nums[curr]=nums[i];
-                    nums[i]=temp2;
-                    i++;
-                    curr++;
-                }
-            }else if(nums[curr]==0){
-                int temp3=nums[curr];
-                nums[curr]=nums[i];
-                nums[i]=temp3;
-                i++;
-                curr++;
-            }else{
-                curr++;
-            }
-        }
-        
-    }
-}
+        int j=nums.length-1;
         int i=0;
+        int curr=0;
     public void sortColors(int[] nums) {
+class Solution {
