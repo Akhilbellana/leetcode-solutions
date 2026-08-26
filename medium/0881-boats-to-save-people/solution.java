bdb1502ee3fc5402d340db74734cec966c883c4a
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-26 23:49:01 +0530

    ✅ Boats to Save People | Medium | Java | Runtime: 38 ms (24.59%) | Memory: 57.6 MB | 2026-08-26

--- medium/0881-boats-to-save-people/solution.java
+++ medium/0881-boats-to-save-people/solution.java
@@ -1,24 +1,24 @@
-            map.put(n,map.getOrDefault(n,0)+1);
-        }
-        int i=0;
-        
-        int c=0;
-        int j=people.length-1;
-        while(i<=j){
-            if(people[i]+people[j]<=limit){
-        }
-        Arrays.sort(people);
-                c++;
-            }else{
-                j--;
-                i++;
-            }
-    }
-                j--;
-                c++;
-        for(int n:people){
-        Map<Integer,Integer>map=new HashMap<>();
-    public int numRescueBoats(int[] people, int limit) {
-class Solution {
-        return c;
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int n:people){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        int c=0;
+        int i=0;
+        int j=people.length-1;
+        Arrays.sort(people);
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                c++;
+                j--;
+                i++;
+            }else{
+                j--;
+                c++;
+            }
+        }
+        return c;
+        
+    }
 }
