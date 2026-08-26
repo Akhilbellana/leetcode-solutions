179e4b958b8651b952abbcba41cdc63c61ad3bfd
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-26 08:39:42 +0530

    ✅ Shortest and Lexicographically Smallest Beautiful String | Medium | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-08-26

--- medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.java
+++ medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.java
@@ -1,24 +1,24 @@
-                }else if(min==j-i+1){
-                     for(int a=0;a<min;a++){
-                    best=s.substring(i,j+1);
-                        if(best.charAt(a)!=s.charAt(a+i)){
-                            if(s.charAt(a+i)=='0'){
-                if(j-i+1<min){
-            if(ones==k){
-                    min=j-i+1;
-            }
-                i++;
-                }
-                    
-                    ones--;
-                if(s.charAt(i)=='1'){
-                                 best=s.substring(i,j+1);
+                    best = s.substring(i, j + 1);
+                    min = j - i + 1;
+                } else if (min == j - i + 1) {
+                    for (int a = 0; a < min; a++) {
+                        if (best.charAt(a) != s.charAt(a + i)) {
+                            if (s.charAt(a + i) == '0') {
+                                best = s.substring(i, j + 1);
+                                break;
                             }
                         }
-                     }
+                    }
                 }
+                if (s.charAt(i) == '1') {
+                    ones--;
+                }
+
+                i++;
             }
             j++;
         }
         return best;
-                                 break;
+
+    }
+                if (j - i + 1 < min) {
