f2cac5dc7b1211576fa3814572430d1da18408d7
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-26 08:42:58 +0530

    ✅ Shortest and Lexicographically Smallest Beautiful String | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-26

--- medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.java
+++ medium/2904-shortest-and-lexicographically-smallest-beautiful-string/solution.java
@@ -1,24 +1,45 @@
-                    best = s.substring(i, j + 1);
-                    min = j - i + 1;
-                } else if (min == j - i + 1) {
-                    for (int a = 0; a < min; a++) {
-                        if (best.charAt(a) != s.charAt(a + i)) {
-                            if (s.charAt(a + i) == '0') {
-                                best = s.substring(i, j + 1);
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (s.charAt(i) == '1') {
-                    ones--;
-                }
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        int ones = 0;
+        int i = 0;
+        int j = 0;
+        int min = Integer.MAX_VALUE;
+        String best = "";
+        while (j < s.length()) {
+            char st = s.charAt(j);
+            if (st == '1') {
+                ones++;
+            }
+            while (ones > k) {
+                if (s.charAt(i) == '1') {
+                    ones--;
 
-                i++;
-            }
-            j++;
-        }
-        return best;
+                }
+                i++;
+            }
+            while (ones == k) {
+                if (j - i + 1 < min) {
+                    best = s.substring(i, j + 1);
+                    min = j - i + 1;
+                } else if (min == j - i + 1) {
+                    for (int a = 0; a < min; a++) {
+                        if (best.charAt(a) != s.charAt(a + i)) {
+                            if (s.charAt(a + i) == '0') {
+                                best = s.substring(i, j + 1);
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (s.charAt(i) == '1') {
+                    ones--;
+                }
 
-    }
-                if (j - i + 1 < min) {
+                i++;
+            }
+            j++;
+        }
+        return best;
+
+    }
+}
