a6e3445d190869640163094b9bf4e3d97cd470ff
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-28 00:48:52 +0530

    ✅ Product of Array Except Self | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-27

--- medium/0238-product-of-array-except-self/solution.java
+++ medium/0238-product-of-array-except-self/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] prefix=new int[nums.length];
         for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]*nums[i];
+            prefix[i]=prefix[i-1]*nums[i-1];
         }
         return prefix;
         for(int i=nums.length-1;i>=0;i--){
