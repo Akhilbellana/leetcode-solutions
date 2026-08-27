98bd99a1e883e529f2c928fa8a96a36363e60c47
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-27 18:48:34 +0530

    ✅ Number of 1 Bits | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.5 MB | 2026-08-27

--- easy/0191-number-of-1-bits/solution.java
+++ easy/0191-number-of-1-bits/solution.java
@@ -1,10 +1,11 @@
 class Solution {
     public int hammingWeight(int n) {
         int count=0;
-        for(int i=0;i<31;i++){
-            if((n & (1<<i))!=0){
+        while(n>0){
+            if((n&1)==1){
                 count++;
             }
+            n=n>>1;
         }
         return count;
         
