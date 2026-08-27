f8cd06f08206deca86a88c8acdc006a0a718a549
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-27 23:30:55 +0530

    ✅ Boats to Save People | Medium | Java | Runtime: 19 ms (98.83%) | Memory: 56.4 MB | 2026-08-27

--- medium/0881-boats-to-save-people/solution.java
+++ medium/0881-boats-to-save-people/solution.java
@@ -1,9 +1,5 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Map<Integer,Integer>map=new HashMap<>();
-        for(int n:people){
-            map.put(n,map.getOrDefault(n,0)+1);
-        }
         int c=0;
         int i=0;
         int j=people.length-1;
