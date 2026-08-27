289f6fe187660994662b8d75e8d5bb1d7c90ce08
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-27 23:41:45 +0530

    ✅ Counting Bits | Easy | Java | Runtime: 5 ms (22.09%) | Memory: 48.7 MB | 2026-08-27

--- easy/0338-counting-bits/solution.java
+++ easy/0338-counting-bits/solution.java
@@ -1,16 +1,20 @@
-        for (int i = 1; i <= n; i++) {
-            if ((val & 1) != 0) {
-                count++;
-            }
-            val=val>>1;
-            while(val>0){
-            }
-            int count = 0;
-            int val=i;
-            a[i] = count;
-        }
-        a[0]=0;
-        int[] a = new int[n + 1];
-        
-    public int[] countBits(int n) {
-class Solution {
+class Solution {
+    public int[] countBits(int n) {
+        
+        int[] a = new int[n + 1];
+        a[0]=0;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            int val=i;
+            while(val>0){
+            if ((val & 1) != 0) {
+                count++;
+            }
+            val=val>>1;
+            }
+            a[i] = count;
+        }
+        return a;
+
+    }
+}
