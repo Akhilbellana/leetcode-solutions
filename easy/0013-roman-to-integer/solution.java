2b11a87d5dcc05e3327ce0aefe6a08f8b63bbd6c
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-28 10:50:53 +0530

    ✅ Roman to Integer | Easy | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-08-28

---
+++ easy/0013-roman-to-integer/solution.java
@@ -0,0 +1,19 @@
+                num+=1;
+            }else if(ch=='V'){
+        }
+        
+                num+=5;
+            }else if(ch=='X'){
+                num+=10;
+            }else if(ch=='L'){
+                num+=50;
+            }else if(ch=='C'){
+                num+=100;
+            }else if(ch=='D'){
+                num+=500;
+            }else if(ch=='M'){
+                num+=1000;
+            }
+    }
+        return num;
+}
