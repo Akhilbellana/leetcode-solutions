965a332f89c6e2ee46621554aec264f02848b72a
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-28 23:30:55 +0530

    ✅ Is Subsequence | Easy | Java | Runtime: 2 ms (73.22%) | Memory: 42.9 MB | 2026-08-28

--- easy/0392-is-subsequence/solution.java
+++ easy/0392-is-subsequence/solution.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        String ans="";
         int i=0;
         int j=0;
         while(i<s.length()&&j<t.length()){
             if(s.charAt(i)==t.charAt(j)){
-                ans+=t.charAt(j);
                 i++;
                 j++;
             }else{
                 j++;
             }
         }
-        if(s.equals(ans)){
+         if(i==s.length()){
             return true;
-        }
+         }
         return false;
         
     }
