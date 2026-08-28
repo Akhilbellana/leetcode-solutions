e89983524d9a73aebaf36b7c6838882ba64c06ab
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-28 14:52:44 +0530

    ✅ Flipping an Image | Easy | Java | Runtime: 1 ms (47.07%) | Memory: 45.4 MB | 2026-08-28

--- easy/0832-flipping-an-image/solution.java
+++ easy/0832-flipping-an-image/solution.java
@@ -1,14 +1,27 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for (int i = 0; i < image.length; i++) {
+            int left = 0;
+            int right = image[i].length - 1;
+            while (left < right) {
+                int temp=image[i][left];
+                image[i][left]=image[i][right];
+                image[i][right]=temp;
+                left++;
+                right--;
 
-            }
-                int temp=image[i][left];
-                image[i][left]=image[i][right];
-                image[i][right]=temp;
-        }
-                left++;
-                right--;
-            while (left < right) {
-            int right = image[i].length - 1;
-            int left = 0;
-        for (int i = 0; i < image.length; i++) {
-    public int[][] flipAndInvertImage(int[][] image) {
-class Solution {
+            }
+        }
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<image.length;j++){
+                if(image[i][j]==0){
+                    image[i][j]=1;
+                }else{
+                    image[i][j]=0;
+                }
+            }
+        }
+        return image;
+
+    }
+}
