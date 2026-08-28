d0224597fe187bdf492f1100f035d01052c380e0
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-28 11:22:12 +0530

    ✅ Roman to Integer | Easy | Java | Runtime: 0 ms | Memory: 42.1 MB | 2026-08-28

--- easy/0013-roman-to-integer/solution.java
+++ easy/0013-roman-to-integer/solution.java
@@ -1,19 +1,23 @@
-                num+=1;
-            }else if(ch=='V'){
-        }
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int num=map.get(s.charAt(s.length()-1));
+        return num;
         
-                num+=5;
-            }else if(ch=='X'){
-                num+=10;
-            }else if(ch=='L'){
-                num+=50;
-            }else if(ch=='C'){
-                num+=100;
-            }else if(ch=='D'){
-                num+=500;
-            }else if(ch=='M'){
-                num+=1000;
+        map.put('X',10);
+        map.put('V',5);
+    public int romanToInt(String s) {
+        Map<Character,Integer>map=new HashMap<>();
+        map.put('I',1);
+        for(int i=s.length()-1;i>0;i--){
+            if(map.get(s.charAt(i))>map.get(s.charAt(i-1))){
+        }
+                num-=map.get(s.charAt(i-1));
+            }else{
+
             }
     }
-        return num;
+                num+=map.get(s.charAt(i-1));
+        num+=map.get(s.charAt(0));
 }
