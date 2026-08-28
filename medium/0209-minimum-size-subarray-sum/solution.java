0ce4df3929fe478c8e5ed38463e9a7485cdd1cc4
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-28 18:39:23 +0530

    ✅ Minimum Size Subarray Sum | Medium | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-08-28

--- medium/0209-minimum-size-subarray-sum/solution.java
+++ medium/0209-minimum-size-subarray-sum/solution.java
@@ -1,28 +1,15 @@
-class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        boolean valid = false;
-        int min = Integer.MAX_VALUE;
-        while (j < nums.length) {
-            sum += nums[j];
-            if (sum >= target) {
-                min = Math.min(min, j - i + 1);
-                valid = true;
-            }
-            while (sum >= target) {
-                sum = sum - nums[i];
-                i++;
-                if (sum >= target) {
-                    min = Math.min(min, j - i + 1);
-                }
-            }
-            j++;
-        }
-        if (valid) {
-            return min;
-        }
-        return 0;
-    }
-}
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        boolean valid = false;
+        int min = Integer.MAX_VALUE;
+        while (j < nums.length) {
+            sum += nums[j];
+            while (sum >= target) {
+                sum = sum - nums[i];
+                i++;
+            }
+                min = Math.min(min, j - i + 1);
+                valid = true;
