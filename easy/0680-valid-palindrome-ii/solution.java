304ad4eac7844544b11ddc5fcfe0c9d5486fcf5e
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-29 08:47:17 +0530

    ✅ Valid Palindrome II | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-29

--- easy/0680-valid-palindrome-ii/solution.java
+++ easy/0680-valid-palindrome-ii/solution.java
@@ -1,19 +1,16 @@
-class Solution {
-    public boolean validPalindrome(String s) {
-        int i=0;
-        int j=s.length()-1;
-        boolean valid=true;
-        while(i<j && valid){
-            if(s.charAt(i)!=s.charAt(j)){
-        }
-        
-                valid=false;
-            }
-            i++;
-            j--;
-        if(j-i==1){
-            return true;
-        }
-    }
-        return false;
-}
+                 right=j-1;
+                while(left<right){
+                     if(s.charAt(left)!=s.charAt(right)){
+                        break;
+                        validright=false;
+                 left=i;
+                }
+                
+                    right--;
+                    left++;
+                    }
+                        break;
+                        validleft=false;
+                 boolean validright=true;
+                    if(s.charAt(left)!=s.charAt(right)){
+                while(left<right){
