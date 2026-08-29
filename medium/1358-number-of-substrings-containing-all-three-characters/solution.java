d52bd22a6da487fea8782cbf63155afa332d37b8
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-29 16:54:42 +0530

    ✅ Number of Substrings Containing All Three Characters | Medium | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-08-29

--- medium/1358-number-of-substrings-containing-all-three-characters/solution.java
+++ medium/1358-number-of-substrings-containing-all-three-characters/solution.java
@@ -1,9 +1,5 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        Map<Character,Integer>map=new HashMap<>();
-        map.put('a',0);
-        map.put('b',0);
-        map.put('c',0);
         for(int i=0;i<s.length();i++){
             for(int j=i;j<s.length();j++){
                 map.put(s.charAt(j),map.get(s.charAt(j))+1);
@@ -13,3 +9,7 @@ class Solution {
                         break;
                     }
         int count=0;
+        Map<Character,Integer>map=new HashMap<>();
+        map.put('a',0);
+        map.put('b',0);
+        map.put('c',0);
