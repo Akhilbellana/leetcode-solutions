2e55c0d526e071affdefd12371f00a003d778f67
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-30 23:24:17 +0530

    ✅ Maximum Product Subarray | Medium | Java | Runtime: 275 ms (5.07%) | Memory: 47.2 MB | 2026-08-30

--- medium/0152-maximum-product-subarray/solution.java
+++ medium/0152-maximum-product-subarray/solution.java
@@ -1,14 +1,14 @@
-class Solution {
-    public int maxProduct(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            int product=1;
-            for(int j=i;j<nums.length;j++){
-                product=product*nums[j];
-            }
-        }
-        
-    }
-                max=Math.max(max,product);
-        return max;
-        int max=0;
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int product=1;
+            for(int j=i;j<nums.length;j++){
+                product=product*nums[j];
+                max=Math.max(max,product);
+            }
+        }
+        return max;
+        
+    }
 }
