514a2292a4bfe51a0aef16013f6a60a467c990c7
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-30 23:14:55 +0530

    ✅ Perfect Number | Easy | Java | Runtime: 2123 ms (9.42%) | Memory: 42.1 MB | 2026-08-30

--- easy/0507-perfect-number/solution.java
+++ easy/0507-perfect-number/solution.java
@@ -1,15 +1,15 @@
-class Solution {
-    public boolean checkPerfectNumber(int num) {
-        for(int i=1;i<num;i++){
-            if(num%i==0){
-                sum+=i;
-            }
-        }
-        
-        if(sum==num){
-            return true;
-        }
-    }
-        return false;
-        int sum=0;
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0){
+                sum+=i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+        
+    }
 }
