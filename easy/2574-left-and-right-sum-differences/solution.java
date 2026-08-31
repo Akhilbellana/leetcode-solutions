c18480064bebf6cd492cfabce32b42458a6f33ac
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-31 10:45:11 +0530

    ✅ Left and Right Sum Differences | Easy | Java | Runtime: 2 ms (99.14%) | Memory: 46.6 MB | 2026-08-31

--- easy/2574-left-and-right-sum-differences/solution.java
+++ easy/2574-left-and-right-sum-differences/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-        int[] prefix=new int[nums.length+1];
-        prefix[0]=0;
-        for(int i=0;i<nums.length;i++){
-            prefix[i+1]=prefix[i]+nums[i];
+        int total = 0;
+        for (int n : nums) {
+            total += n;
         }
-        int[]a =new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-             prefix[i]=Math.abs(prefix[nums.length]-prefix[i+1]-prefix[i]);
-             a[i]=prefix[i];
+        int[] a = new int[nums.length];
+        int left = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            a[i] = Math.abs((total - left - nums[i]) - left);
+            left = left + nums[i];
         }
         return a;
 
-        
     }
 }
