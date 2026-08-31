ba73a1bca4d84314127ef04e6aa0346a87e636a4
Akhil Bellana <akhilbellana22@gmail.com>
2026-08-31 15:11:47 +0530

    ✅ Maximum Subarray | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-08-31

--- medium/0053-maximum-subarray/solution.java
+++ medium/0053-maximum-subarray/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                max=Math.max(max,sum);
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum >= 0) {
+                max = Math.max(max, sum);
+            } else {
+                sum = 0;
             }
         }
         return max;
-        
+
     }
 }
