80a75201790b7f607706caac859bca357d61ce84
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-06 15:52:27 +0530

    ✅ Range Sum Query 2D - Immutable | Medium | Java | Runtime: 1 ms | Memory: 42.6 MB | 2026-09-06

--- medium/0304-range-sum-query-2d-immutable/solution.java
+++ medium/0304-range-sum-query-2d-immutable/solution.java
@@ -1,9 +1,11 @@
+            }
+        }
         
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        }
-        return prefix[row2+1][col2+1]-prefix[row1][col2+1]-prefix[row2+1][col1]+prefix[row1]
+        return prefix[row2+1][col2+1]-prefix[row1][col2+1]-prefix[row2+1][col1]+prefix
+        [row1][col1];
         
     }
 }
@@ -12,6 +14,4 @@
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
  * int param_1 = obj.sumRegion(row1,col1,row2,col2);
-            }
-        [col1];
  */
