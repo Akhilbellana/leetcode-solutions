5cba19c3b22d9af41d79cf0f377c687fb497872e
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-06 12:44:32 +0530

    ✅ Unique Number of Occurrences | Easy | Java | Runtime: 2 ms (98.03%) | Memory: 43.5 MB | 2026-09-06

--- easy/1207-unique-number-of-occurrences/solution.java
+++ easy/1207-unique-number-of-occurrences/solution.java
@@ -1,16 +1,16 @@
-class Solution {
-    public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer>map=new HashMap<>();
-        for(int n:arr){
-            map.put(n,map.getOrDefault(n,0)+1);
-        }
-        for(int value:map.values()){
-        return true;
-        Set<Integer>set=new HashSet<>();
-            if(set.contains(value)){
-        }
-                return false;
-            }
-    }
-            set.add(value);
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int n:arr){
+            map.put(n,map.getOrDefault(n,0)+1);
+        }
+        Set<Integer>set=new HashSet<>();
+        for(int value:map.values()){
+            if(set.contains(value)){
+                return false;
+            }
+            set.add(value);
+        }
+        return true;
+    }
 }
