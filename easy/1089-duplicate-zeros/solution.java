dc831a096577561869c7a689fddf6791eeb23290
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-06 10:56:04 +0530

    ✅ Duplicate Zeros | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-09-06

---
+++ easy/1089-duplicate-zeros/solution.java
@@ -0,0 +1,13 @@
+        for(int n:arr){
+            if(n==0){
+                list.add(n);
+            }
+        }
+        
+            list.add(n);
+        for(int i=0;i<arr.length;i++){
+            arr[i]=list.get(i);
+        }
+    }
+
+}
