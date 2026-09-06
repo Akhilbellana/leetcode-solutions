1fde7fb9a73ff4ab05a85aff9e63461057b4dc68
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-06 10:56:30 +0530

    ✅ Duplicate Zeros | Easy | Java | Runtime: 3 ms (37.71%) | Memory: 46.5 MB | 2026-09-06

--- easy/1089-duplicate-zeros/solution.java
+++ easy/1089-duplicate-zeros/solution.java
@@ -1,13 +1,14 @@
-        for(int n:arr){
-            if(n==0){
-                list.add(n);
-            }
-        }
-        
-            list.add(n);
-        for(int i=0;i<arr.length;i++){
-            arr[i]=list.get(i);
-        }
-    }
-
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int n : arr) {
+            if (n == 0) {
+                list.add(n);
+            }
+            list.add(n);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+    }
 }
