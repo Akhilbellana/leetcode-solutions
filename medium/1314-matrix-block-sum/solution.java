585dcddb9b5394a0659c55c378592a5a01581d2c
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-07 11:03:39 +0530

    ✅ Matrix Block Sum | Medium | Java | Runtime: 0 ms | Memory: 42.9 MB | 2026-09-07

---
+++ medium/1314-matrix-block-sum/solution.java
@@ -0,0 +1,23 @@
+        int[][]answer=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int r1=Math.max(i-k,0);
+                int r2=Math.min(i+k,n-1);
+                int c1=Math.max(j-k,0);
+                int c2=Math.min(j+k,m-1);
+                answer[i][j]=prefix[r2+1][c2+1]-prefix[r1][c2+1]-prefix[r2+1][c1]-prefix
+            }
+                [r1][c1];
+        }
+        return answer;
+                [j-1];
+            }
+        }
+                prefix[i][j]=mat[i-1][j-1]+prefix[i-1][j]+prefix[i][j-1]-prefix[i-1]
+        int[][] prefix=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+        int n=mat.length;
+        int m=mat[0].length;
+class Solution {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
