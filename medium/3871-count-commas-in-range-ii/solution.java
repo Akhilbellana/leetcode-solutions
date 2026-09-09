c9dac011a66736490dc91512f751b3819d295215
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-09 13:11:39 +0530

    ✅ Count Commas in Range II | Medium | Java | Runtime: 1 ms (99.36%) | Memory: 42.1 MB | 2026-09-09

--- medium/3871-count-commas-in-range-ii/solution.java
+++ medium/3871-count-commas-in-range-ii/solution.java
@@ -7,27 +7,27 @@ class Solution {
 
             if (num <= 999999) {
                 count += (num - 999);
-                num = 999;
-                
+                num = num - (num - 999);
+
             } else if (num <= 999999999) {
                 count += (num - 999999) * 2;
-                num = 999999;
+                num = num - (num - 999999);
 
             } else if (num <= 999999999999L) {
                 count += (num - 999999999) * 3;
-                num = 999999999;
+                num = num - (num - 999999999);
 
             } else if (num <= 999999999999999L) {
                 count += (num - 999999999999L) * 4;
-                num = 999999999999L;
+                num = num - (num - 999999999999L);
 
             } else if (num <= 999999999999999999L) {
                 count += (num - 999999999999999L) * 5;
-                num = 999999999999999L;
+                num = num - (num - 999999999999999L);
 
             } else {
                 count += (num - 999999999999999999L) * 6;
-                num = 999999999999999999L;
+                num = num - (num - 999999999999999999L);
             }
         }
 
