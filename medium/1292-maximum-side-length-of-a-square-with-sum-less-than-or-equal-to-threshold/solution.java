d5e19f4a79d8c3259db95c6b65778800d55c8369
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-10 23:33:40 +0530

    ✅ Maximum Side Length of a Square with Sum Less than or Equal to Threshold | Medium | Java | Runtime: 0 ms | Memory: 42.4 MB | 2026-09-10

---
+++ medium/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSideLength(int[][] mat, int threshold) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int[][]prefix=new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                prefix[i][j]=mat[i-1][j-1]+prefix[i][j-1]+prefix[i-1][j]-prefix
+                [i-1][j-1];
+            }
+        }
+        for(int r=0;r+size<=n;r++){
+            for(int c=0;c+size<=m;c++){
+        for(int size=1;size<Math.min(n,m);size++){
+        int max=0;
+                int r1=r;
+                int r2=r+size-1;
+                int c1=c;
+                int c2=c+size-1;
+                int sum=prefix[r2+1][c2+1]-prefix[r1][c2+1]-prefix[r2+1][c1]
