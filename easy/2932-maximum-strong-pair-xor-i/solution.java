296601b8d0166309262a60a8ba91dd5d6d915eec
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-12 07:41:24 +0530

    ✅ Maximum Strong Pair XOR I | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-09-12

---
+++ easy/2932-maximum-strong-pair-xor-i/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumStrongPairXor(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i;j<=i+1;j++){
+
+            }
+        }
+        
+                if(nums[i]-nums[j]<=Math.min(nums[i],nums[j])){
+                    val=Math.max(val,nums[i]^nums[j]);
+                }
+    }
+        return val;
+        int val=0;
+}
