a97345c1c8a7d4b1a274e350aaa5ca592f082447
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-13 15:37:30 +0530

    ✅ Minimum Swaps to Move Zeros to End | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-09-13

--- easy/3936-minimum-swaps-to-move-zeros-to-end/solution.java
+++ easy/3936-minimum-swaps-to-move-zeros-to-end/solution.java
@@ -1,12 +1,19 @@
 class Solution {
     public int minimumSwaps(int[] nums) {
         for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]!=0){
+            if(nums[i]!=0 && zeros!=0){
                 count++;
             }
         }
         return count;
 
-    }
         int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+        }
+                zeros++;
+            }
+    }
+        int zeros=0;
+                zeros--;
 }
