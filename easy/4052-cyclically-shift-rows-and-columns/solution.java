1ae8a09b44d3a1f6af071bee4f606c289b77b083
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-13 14:50:40 +0530

    ✅ Cyclically Shift Rows and Columns | Easy | Java | Runtime: 0 ms | Memory: 42.1 MB | 2026-09-13

--- easy/4052-cyclically-shift-rows-and-columns/solution.java
+++ easy/4052-cyclically-shift-rows-and-columns/solution.java
@@ -2,15 +2,14 @@ class Solution {
     public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                grid[i][j]=grid[i][(j - colShift[j] + n) % n];
+                grid[i][j]=grid[i][(j - rowShift[j] + n) % n];
             }
         }
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                grid[i][j]=grid[(i-rowShift[i]+n)%n][j];
+                grid[i][j]=grid[(i-colShift[i]+n)%n][j];
             }
         }
         
     }
         return grid;
-}
