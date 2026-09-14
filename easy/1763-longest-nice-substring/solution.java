9546752d07b31e35f4e5b2767439790f129ef19e
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-14 12:14:04 +0530

    ✅ Longest Nice Substring | Easy | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-09-14

--- easy/1763-longest-nice-substring/solution.java
+++ easy/1763-longest-nice-substring/solution.java
@@ -2,23 +2,23 @@ class Solution {
     public String longestNiceSubstring(String s) {
         String ans="";
         for(int i=0;i<s.length();i++){
+        Set<Character>set=new HashSet<>();
             for(int j=i;j<s.length();j++){
+                set.add(s.charAt(j));
+                boolean valid=true;
+                for(char ch:set){
                 if(!set.contains(Character.toLowerCase(s.charAt(j)))&&!set.contains(Character.
                 toUpperCase(s.charAt(j)))){
+                    valid=false;
+                    break;
+                }
+                }   
                     if(valid && ans.length()<j-i+1){
-                        ans=s.substring(i,j);
+                        ans=s.substring(i,j+1);
                     }
                 
             }
         }
         return ans;
     }
-        Set<Character>set=new HashSet<>();
-                set.add(s.charAt(j));
-                for(char ch:set){
-                }
-                    valid=false;
-                    break;
-                }   
-                boolean valid=true;
 }
