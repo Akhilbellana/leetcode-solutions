7b86037541c0478f0752c84aaf25a13a433a6d19
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-19 16:57:23 +0530

    ✅ Count Number of Nice Subarrays | Medium | Java | Runtime: 44 ms (18.83%) | Memory: 56.2 MB | 2026-09-19

--- medium/1248-count-number-of-nice-subarrays/solution.java
+++ medium/1248-count-number-of-nice-subarrays/solution.java
@@ -1,32 +1,21 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int odd=0;
+        Map<Integer,Integer>map=new HashMap<>();
+        map.put(0,1);
+        int sum=0;
         int count=0;
-        int i=0;
-        int j=0;
-        while(j<nums.length){
-            if(nums[j]%2!=0){
-                odd++;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                sum+=0;
+            }else{
+                sum+=1;
             }
-            while(odd>k){
-                if(nums[i]%2!=0){
-                    odd--;
-                }
-                i++;
+            if(map.containsKey(sum-k)){
+                count+=map.get(sum-k);
             }
-            if(odd==k){
-                int temp=i;
-                int even=0;
-                while(nums[temp]%2==0){
-                    even++;
-                    temp++;
-                }
-                count+=even+1;
-            }
-            j++;
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return count;
 
-        
     }
 }
