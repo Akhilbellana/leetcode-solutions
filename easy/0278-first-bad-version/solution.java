0fd85d42aa585e7f57d7b86f6df26f64c1b0afa6
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-21 22:47:40 +0530

    ✅ First Bad Version | Easy | Java | Runtime: 14 ms (49.59%) | Memory: 42.1 MB | 2026-09-21

--- easy/0278-first-bad-version/solution.java
+++ easy/0278-first-bad-version/solution.java
@@ -5,16 +5,15 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int left=0;
         int right=n;
-        int ans=-1;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(isBadVersion(mid)){
-                ans=mid;
+
                 right=mid-1;
             }else{
                 left=mid+1;
             }
         }
-        return ans;
+        return left;
     }
 }
