b8d36b5b5963e00e9648d8d6fc2d8399a82ec127
Akhil Bellana <akhilbellana22@gmail.com>
2026-09-24 01:03:16 +0530

    ✅ Count Number of Pairs With Absolute Difference K | Easy | Java | Runtime: 8 ms (11.12%) | Memory: 46.9 MB | 2026-09-23

--- easy/2006-count-number-of-pairs-with-absolute-difference-k/solution.java
+++ easy/2006-count-number-of-pairs-with-absolute-difference-k/solution.java
@@ -2,12 +2,12 @@ class Solution {
     public int countKDifference(int[] nums, int k) {
         int count=0;
         Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j]-nums[i]==k){
-                    count++;
-                }
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey((nums[i]-k))){
+                count+=map.get((nums[i]-k));
             }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);   
         }
         return count;
         
